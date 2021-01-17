147fdc6327451d8b6412be774431efebf82c6449
squarematr1x <sasu.pirttipera@protonmail.com>
2021-01-17 21:48:42 +0200

    Added early return

--- src/Pacman.cpp
+++ src/Pacman.cpp
@@ -38,22 +38,26 @@ void Pacman::updatePos(Direction dir, World& world)
 
 void Pacman::validatePos(position next_pos, World& world)
 {
-	int x = static_cast<int>(std::round(m_pos.x));
-	int y = static_cast<int>(std::round(m_pos.y));
 	int next_x = static_cast<int>(std::round(next_pos.x));
 	int next_y = static_cast<int>(std::round(next_pos.y));
 
-	if (!world.isWall(next_y, next_x, true))
-	{
-		updateWorld(world, next_y, next_x);
+	if (world.isWall(next_y, next_x))
+		return;
 
-		world.updatePlayerPos(y, x, next_y, next_x);
+	updateWorld(world, next_y, next_x);
 
-		SDL_Rect dest = { 32 * next_x, 32 * next_y, 32, 32 };
-		m_sprite->update(dest, m_dir);
+	int x = static_cast<int>(std::round(m_pos.x));
+	int y = static_cast<int>(std::round(m_pos.y));
 
-		m_pos = next_pos;
-	}
+	world.updatePlayerPos(y, x, next_y, next_x);
+
+	// int dest_x = static_cast<int>(((m_pos.x + next_pos.x) / 2) * 32); // This doesn't work when going through 'pipe' (pacman is rendered at the center)
+	// int dest_y = static_cast<int>(((m_pos.y + next_pos.y) / 2) * 32);
+
+	SDL_Rect dest = { next_x*32, next_y*32, 32, 32 };
+	m_sprite->update(dest, m_dir);
+
+	m_pos = next_pos;
 }
 
 void Pacman::updateWorld(World& world, int y, int x)
