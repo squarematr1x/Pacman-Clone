2ac06fbf91d8a070c22d554a9b7d4621db95fee3
squarematr1x <sasu.pirttipera@protonmail.com>
2021-02-18 21:17:15 +0200

    Added new constants for sprite sheet coordinates

--- src/Constants.h
+++ src/Constants.h
@@ -1,5 +1,14 @@
 #pragma once
 
+// Map and tile length
 constexpr int tile_len{ 32 };
 constexpr int map_w{ 23 };
 constexpr int map_h{ 28 };
+
+// Texture atlas coordinates
+constexpr int big_point_x{ 128 };
+constexpr int big_point_y{ 128 };
+constexpr int point_x{ 160 };
+constexpr int point_y{ 128 };
+constexpr int berry_x{ 192 };
+constexpr int berry_y{ 128 };
